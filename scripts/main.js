4951daab414304f76f82eefaba9ec7175decd6e2
Malo Lefort <mlefort@bess-f2r1s17.clusters.42paris.fr>
2025-03-07 18:12:45 +0100

    feat(openclassroom): JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+// lancerJeu();
+
+let inputUser = document.getElementById("inputEcriture");
+console.log(inputUser);
+
+let BtnValid = document.getElementById("btnValiderMot");
+console.log(BtnValid);
+
+let zoneUserPrompt = document.querySelector(".zoneProposition");
+console.log(zoneUserPrompt);
+
+let score = document.querySelector(".zoneScore span");
+console.log(score);
+
+let BtnRadio = document.querySelectorAll(".optionSource input");
+console.log(BtnRadio)
